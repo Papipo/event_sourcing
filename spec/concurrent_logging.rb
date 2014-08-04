a51b1a7fadb0472494ca7968e3a2455cc689286e
Rodrigo Alvarez <papipo@gmail.com>
2014-08-05 01:01:54 +0200

    Fix test logging

--- spec/concurrent_logging.rb
+++ spec/concurrent_logging.rb
@@ -1,6 +1,9 @@
 require "concurrent"
 require "logger"
 
+$logger = Logger.new(File.dirname(__FILE__) + '/../log/test.log')
+$logger.level = Logger::DEBUG
+
 Concurrent.configuration.logger = lambda do |level, progname, message = nil, &block|
-  Logger.new(File.dirname(__FILE__) + '/../log/test.log').add Logger::INFO, message, progname, &block
+  $logger.add level, message, progname, &block
 end
